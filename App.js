db924342db7a7029d4ef1b585b1027b796bd5df7
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-11-09 12:33:15 +0000

    header

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import Header from './components/header';
 
 export default function App() {
+  const [data, setData] = useState(
+    [
+      {text: 'buy me a cofffe', id: 1},
+      {text: "create an app", id: 2},
+      {text: "play on the switch", id: 3},
+  ]
+  )
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Header/>
+        <View style={styles.list}>
+        <FlatList
+        data={data}
+        renderItem={({item})=>(
+          <Text style={styles.listText}>{item.text}</Text>
+  )}
+        />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,4 +33,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  list: {
+    padding: 30,
+  }
 });
---
+++ components/header.js
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet} from "react-native"
+
+const Header = () =>{
+    return(
+        <View style={styles.header}>
+            <Text style={styles.text}>My Todos</Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    header:{
+        backgroundColor: "orange",
+        paddingTop: 30,
+        height: 80,
+    },
+    text:{
+        textAlign: "center",
+        color: "#fff",
+        fontSize: 20,
+        fontWeight: "bold",
+    }
+})
+
+export default Header;
